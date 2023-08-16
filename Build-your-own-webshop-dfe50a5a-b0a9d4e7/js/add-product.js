5a9d0f5b855fae87e020ce9d867e90e80595a4f7
karelkruijer <142407338+karelkruijer@users.noreply.github.com>
2023-08-16 15:12:45 +0200

    Add files via upload

---
+++ Build-your-own-webshop-dfe50a5a-b0a9d4e7/js/add-product.js
@@ -0,0 +1,32 @@
+const nameProductAdd = document.getElementById('AddproductNaam');
+const priceProductAdd = document.getElementById('Addproductprijs');
+const imgProductAdd = document.getElementById('addProductImg');
+
+let producten = JSON.parse(localStorage.getItem('products')) || [];
+
+function addProduct() {
+    let newName = nameProductAdd.value;
+    let newPrice = parseFloat(priceProductAdd.value);
+    let newImg = imgProductAdd.value;
+
+    let highestId = 0;
+    producten.forEach(product => {
+        let currentId = parseInt(product.id); 
+        if (currentId > highestId) {
+            highestId = currentId;
+        }
+    });
+    let newId = highestId + 1;  
+
+    let newProduct = {
+        color: newName,
+        price: newPrice,
+        id: newId,
+        img: newImg
+    };
+    producten.push(newProduct);
+
+    localStorage.setItem('products', JSON.stringify(producten));
+}
+
+document.getElementById('addproduct').addEventListener('click', addProduct);
